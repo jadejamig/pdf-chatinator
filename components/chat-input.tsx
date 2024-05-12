4e5235218798aefd6a3bbcbc1575cf27ea59fa39
Jade Jamig <jadejamigdev@gmail.com>
2024-05-13 07:52:45 +0800

    16px text input

--- components/chat-input.tsx
+++ components/chat-input.tsx
@@ -8,7 +8,7 @@ import { SendHorizonal } from 'lucide-react'
 const ChatInput = () => {
   return (
     <div className="flex w-full gap-2 ">
-      <Textarea autoFocus rows={1} placeholder="Ask me something." className='resize-none' />
+      <Textarea autoFocus rows={1} placeholder="Ask me something." className='resize-none text-base' />
       <Button size='sm' className='flex gap-x-2 justify-center items-center'>
         Send
         <SendHorizonal className='h-4 w-4'/>
