948224a656b8d7ab77a235344a134e4d59dd506d
Jade Jamig <jadejamigdev@gmail.com>
2024-05-14 06:35:34 +0800

    cursors

--- components/chat-input.tsx
+++ components/chat-input.tsx
@@ -7,12 +7,18 @@ import { SendHorizonal } from 'lucide-react'
 import { ChatContext } from '@/Context/chat-context'
 
 const ChatInput = () => {
-  const { addMessage, handleInputChange, message } = useContext(ChatContext)
+  const { addMessage, handleInputChange, message, isLoading } = useContext(ChatContext)
 
   return (
     <div className="flex w-full gap-2 items-end">
-      <Textarea autoFocus rows={1} placeholder="Ask me something." className='resize-none text-base' onChange={handleInputChange} value={message}/>
-      <Button className='flex gap-x-2 justify-center items-center px-6' onClick={addMessage}>
+      <Textarea autoFocus rows={1} 
+                placeholder="Ask me something." 
+                className='resize-none text-base' 
+                onChange={handleInputChange} 
+                value={message} 
+                disabled={isLoading}
+      />
+      <Button className='flex gap-x-2 justify-center items-center px-6' onClick={addMessage} disabled={isLoading}>
         Send
         <SendHorizonal className='h-4 w-4'/>
       </Button>
