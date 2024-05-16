2dfe54d1ed653351d4f1e12ff55b8af47919242f
Jade Jamig <jadejamigdev@gmail.com>
2024-05-16 13:09:23 +0800

    adjusted mobile view

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ const Dashboard = async () => {
   const retrievedfiles = await getUserFiles();
 
   return (
-    <main className="flex flex-1 w-full justify-center items-center h-dvh max-h-dvh bg-zinc-950 bg-[url('/grid.svg')] p-10">
+    <main className="flex flex-1 w-full justify-center items-center h-dvh max-h-dvh bg-zinc-950 bg-[url('/grid.svg')] p-4 md:p-10">
         <div className='flex flex-col max-w-6xl w-full h-full gap-y-6'>
 
             <NavigationBar/>
