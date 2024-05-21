ea619abbe4b219c9a94156982f28b10bd1233713
Jade Jamig <jadejamigdev@gmail.com>
2024-05-21 10:50:58 +0800

    donut

--- app/file/[id]/pdf-render.tsx
+++ app/file/[id]/pdf-render.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
-import { FileText, LoaderCircle, LoaderPinwheel } from 'lucide-react';
+import { Donut, FileText, LoaderCircle, LoaderPinwheel } from 'lucide-react';
 import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import { useResizeDetector } from "react-resize-detector";
@@ -29,7 +29,7 @@ const PdfRenderPage = ({ pdfUrl }: PdfRenderPageProps) => {
     return (
       <div className='flex flex-col gap-y-2 justify-center items-center h-[calc(100dvh-7rem)] max-h-calc(100dvh-7rem)]
                       w-full text-zinc-50'>
-          <LoaderCircle className='w-5 h-5 animate-spin font-light'/>
+          <Donut className='w-5 h-5 animate-spin font-light'/>
           <p>{"Please wait while I fetch your file ✌🏻"}</p>
       </div>
     )
